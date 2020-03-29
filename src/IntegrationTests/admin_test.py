2b38966e8ad4139a1e16e25bc9b95e8858cab0a0
Michael <michael.liang1110@gmail.com>
2020-03-29 22:16:22 +1100

    made it more pylint 6/10

--- src/IntegrationTests/admin_test.py
+++ src/IntegrationTests/admin_test.py
@@ -29,7 +29,7 @@ def test_invalid_token(new_user, new_user_2):
     with pytest.raises(AccessError):
         permission_change(new_user['token'], new_user_2['u_id'], 1)
 
-def test_not_owner(new_user, new_user_2, new_user_3):
+def test_not_owner(new_user_2, new_user_3):
     '''tests that a normal user cannot change permission - gives access error'''
     with pytest.raises(AccessError):
         permission_change(new_user_2['token'], new_user_3['u_id'], 1)
@@ -47,17 +47,17 @@ def test_invalid_user(new_user):
 
 def test_member_becoming_owner(new_user, new_user_2):
     '''tests that a user is given owner permissions by another owner'''
-    permission_change(new_user['token'],new_user_2['u_id'], 1)
-    assert get_users()[new_user_2['u_id']]['is_owner'] == True   
+    permission_change(new_user['token'], new_user_2['u_id'], 1)
+    assert get_users()[new_user_2['u_id']]['is_owner'] is True
 
 def test_owner_becoming_member(new_user, new_user_2):
     '''tests that an owner loses owner permissions and becomes a normal user'''
-    permission_change(new_user['token'],new_user_2['u_id'], 1)
-    assert get_users()[new_user_2['u_id']]['is_owner'] == True
-    permission_change(new_user['token'],new_user_2['u_id'], 2)
-    assert get_users()[new_user_2['u_id']]['is_owner'] == False 
+    permission_change(new_user['token'], new_user_2['u_id'], 1)
+    assert get_users()[new_user_2['u_id']]['is_owner'] is True
+    permission_change(new_user['token'], new_user_2['u_id'], 2)
+    assert get_users()[new_user_2['u_id']]['is_owner'] is False
 
 def test_no_permission_change(new_user, new_user_2):
     '''tests that permissions don't change if a member is 'given' member permissions'''
-    permission_change(new_user['token'],new_user_2['u_id'], 2)
-    assert get_users()[new_user_2['u_id']]['is_owner'] == False
+    permission_change(new_user['token'], new_user_2['u_id'], 2)
+    assert get_users()[new_user_2['u_id']]['is_owner'] is False
