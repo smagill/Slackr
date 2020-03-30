0870b88c9bcdda0f5b5ea9682ca20c8d8325831c
Michael <michael.liang1110@gmail.com>
2020-03-30 16:20:28 +1100

    pylint 10/10, still passes all written tests

--- src/search.py
+++ src/search.py
@@ -1,14 +1,16 @@
-from src.global_variables import get_channels
+'''search function to return a collection of messages that contain a certain string'''
 from src.utils import check_token
 from src.channels import channels_list
 
 
 def search(token, query_str):
+    '''given a string, it returns all messages sent containing that string'''
     search_results = []
+    check_token(token) # checking it's a valid user
     user_channels = channels_list(token)
 
-    for id_channel in user_channels[channel_id]:
-        messages = user_channel[id_channel]['messages']
+    for id_channel in user_channels['channel_id']:
+        messages = user_channels[id_channel]['messages']
         for id_message in messages['message_id']:
             message = messages[id_message]['message']
             if query_str in message:
